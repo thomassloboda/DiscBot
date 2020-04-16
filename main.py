63194420ac0919ef62e3c3e353e1255606f1d6e1
Thomas Sloboda <thomas@sloboda.fr>
2020-04-16 08:15:46 +0200

    feat(join_register): allow to join or register a tornament

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 from discord.ext import commands
+from discord.utils import get
 from dotenv import load_dotenv
 
 import sqlite3
@@ -9,12 +10,14 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 DB_PATH = os.getenv('DB_PATH')
+JOIN_ROLE = os.getenv('JOIN_ROLE')
+COMPETITOR_ROLE = os.getenv('COMPETITOR_ROLE')
 connection = sqlite3.connect(DB_PATH)
 dbInstance = connection.cursor()
 
 bot = commands.Bot(command_prefix='!')
 
-dbInstance.execute("CREATE TABLE IF NOT EXISTS hashes (user STRING PRIMARY KEY, hash STRING)")
+dbInstance.execute("CREATE TABLE IF NOT EXISTS hashes (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id STRING, channel_id STRING, user_name STRING, hash STRING)")
 connection.commit()
 
 @bot.event
@@ -27,15 +30,69 @@ async def on_ready():
         f'{guild.name}(id: {guild.id})\n'
     )
 
-@bot.command(name="tornament",pass_context=True)
-async def tornament(ctx, action, *args):
+    shouldCreateJoin = True
+    shouldCreateCompetitor = True
+    for role in guild.roles:
+        if role.name == JOIN_ROLE:
+            shouldCreateJoin = False
+        elif role.name == COMPETITOR_ROLE:
+            shouldCreateCompetitor = False
+    if shouldCreateJoin == True:
+        print("Create role " + JOIN_ROLE)
+        await guild.create_role(name=JOIN_ROLE)
+    if shouldCreateCompetitor == True:
+        print("Create role " + COMPETITOR_ROLE)
+        await guild.create_role(name=COMPETITOR_ROLE)
+
+
+async def join(ctx):
+    for r in ctx.guild.roles:
+        if r.name == JOIN_ROLE:
+            role = r
+            break
     author = ctx.message.author
-    if action == "join":
-        dbInstance.execute("INSERT INTO hashes(user,hash) VALUES (?,?)", (author.display_name, args[0]))
-        connection.commit()
-        await ctx.send("hello " + author.display_name + ", you've joined with hash " + args[0])
+    if role:
+        await author.add_roles(role)
+        await ctx.send(author.mention + ", you've joined the competitors")
     else:
-        await ctx.send("hello " + author.display_name + ", unknown commands " + args)
+        await ctx.send(author.mention + ", tornament now working at the moment")
+
+
+async def register(ctx, hashcode):
+    for r in ctx.guild.roles:
+        if r.name == COMPETITOR_ROLE:
+            cRole = r
+        elif r.name == JOIN_ROLE:
+            jRole = r
+    author = ctx.message.author
+    if cRole and jRole:
+        dbInstance.execute("INSERT INTO hashes(user_id,channel_id,user_name,hash) VALUES (?,?,?,?)", (author.id, ctx.channel.id, author.display_name, hashcode))
+        connection.commit()
+        await author.remove_roles(jRole)
+        await author.add_roles(cRole)
+        await ctx.send(author.mention + ", you are registered with hash " + hashcode)
+    else:
+        await ctx.send(author.mention + ", tornament now working at the moment")
+
+async def help(ctx):
+    await ctx.message.author.send("""here is the help
+    
+- **!tornament join**: allow you to join the current tornament's competitors list
+    
+- **!tornament register {xxxx}**: allow you to register the current tornament passing your deck hash
+    
+    Have fun, see you soon""")
+
+@bot.command(name="tornament", pass_context=True)
+async def tornament(ctx, action, *args):
+    if action == "join":
+        await join(ctx)
+    elif action == "register":
+        await register(ctx, args[0])
+    elif action == "help":
+        await help(ctx)
+    else:
+        await ctx.send(author.mention + ", unknown commands " + args)
 
 
 bot.run(TOKEN)
